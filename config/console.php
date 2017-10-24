a2ddb1dfbfe908769b9d178dd8591461950b22b8
dasscheman <dasscheman@gmail.com>
2017-10-24 08:15:39 +0200

    Mailer en setuptime toegevoegd, dit is nodig voor de cron.

--- config/console.php
+++ config/console.php
@@ -21,6 +21,17 @@ $config = [
                 ],
             ],
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => YII_ENV_DEV ? true : false,
+            'transport' => require(__DIR__ . '/email.php')
+        ],
+        'setupdatetime' => [
+            'class' => 'app\components\SetupDateTime',
+        ],
         'db' => $db,
     ],
 //    'authManager' => [
