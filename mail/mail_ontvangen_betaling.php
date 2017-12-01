5d2744a05d2c1e740e6bceccf205303bb7b7bc01
dasscheman <dasscheman@gmail.com>
2017-12-01 23:05:40 +0100

    Bevestigingsmail toegevoegd

---
+++ mail/mail_ontvangen_betaling.php
@@ -0,0 +1,27 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/* @var $this \yii\web\View view component instance */
+/* @var $message \yii\mail\BaseMessage instance of newly created mail message */
+
+?>
+
+<!--<html>
+	<head></head>
+	<body>-->
+		Hallo <?php echo  $usersProfiel->voornaam; ?>,<br><br>
+
+        <br>
+        <br>
+        Je betaling is in goede orde ontvangen en verwerkt.
+        <br>
+        <br>
+		Als je vragen hebt kun je mailen naar bar@debison.nl
+        <br>
+        <br>
+		Met vriendelijke groet,<br>
+		<br>
+		Daan Asscheman<br>
+<!-- 	</body>
+</html>-->
