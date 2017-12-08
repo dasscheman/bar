68e7b3da431eb7b5abab180674b07fcdcb4bebf0
dasscheman <dasscheman@gmail.com>
2017-12-08 16:24:06 +0100

    Automatisch betalen toegevoegd.

--- controllers/MollieController.php
+++ controllers/MollieController.php
@@ -7,7 +7,6 @@ use app\models\Transacties;
 use app\controllers\TransactiesController;
 use app\models\Mollie;
 use app\models\BetalingType;
-use Mollie_API_Object_Method;
 use app\models\User;
 use yii\web\NotFoundHttpException;
 
@@ -25,6 +24,7 @@ class MollieController extends TransactiesController
         $behaviors['access']['only'][] = 'betaling';
         $behaviors['access']['only'][] = 'webhook';
         $behaviors['access']['only'][] = 'return-betaling';
+        $behaviors['access']['only'][] = 'automatisch-betaling-updaten';
         $behaviors['access']['rules'][] =
             [
                 'allow' => TRUE,
@@ -34,7 +34,7 @@ class MollieController extends TransactiesController
 
         $behaviors['access']['rules'][] =
             [
-                'actions' => ['webhook', 'betaling', 'return-betaling'],
+                'actions' => ['webhook', 'betaling', 'return-betaling', 'automatisch-betaling-updaten'],
                 'allow' => true,
             ];
         return $behaviors;
@@ -57,69 +57,30 @@ class MollieController extends TransactiesController
     {
         $model = new Mollie;
         if ($model->load(Yii::$app->request->post())) {
-            $user = User::findOne($model->transacties_user_id);
-
+            $model->transacties_user_id = Yii::$app->user->id;
             $model->type_id = BetalingType::getIdealId();
             $model->datum = date('Y-m-d H:i:s');
             $model->status = Transacties::STATUS_ingevoerd;
-            if($model->save()) {
-                try
-                {
-                    /*
-                     * Payment parameters:
-                     *   amount        Amount in EUROs. This example creates a € 27.50 payment.
-                     *   method        Payment method "ideal".
-                     *   description   Description of the payment.
-                     *   redirectUrl   Redirect location. The customer will be redirected there after the payment.
-                     *   webhookUrl    Webhook location, used to report when the payment changes state.
-                     *   metadata      Custom metadata that is stored with the payment.
-                     *   issuer        The customer's bank. If empty the customer can select it later.
-                     */
-                    $payment = $model->mollie->payments->create(array(
-                        "amount"       => $model->bedrag,
-                        "method"       => Mollie_API_Object_Method::IDEAL,
-                        "description"  => $model->omschrijving,
-                        "redirectUrl"  => "https://popupbar.biologenkantoor.nl/index.php?r=mollie/return-betaling&transacties_id={$model->transacties_id}",
-                        "webhookUrl"   => "https://popupbar.biologenkantoor.nl/index.php?r=mollie/webhook",
-//                        "redirectUrl"  => "https://bar.debison.nl/index.php?r=mollie/return?transacties_id={$model->transacties_id}",
-//                        "webhookUrl"   => "https://bar.debison.nl/index.php?r=mollie/webhook",
-                        "metadata"     => array(
-                            "transacties_id" => $model->transacties_id,
-                        ),
-                        "issuer"       => !empty($model->issuer) ? $model->issuer : NULL
-                    ));
-                    /*
-                     * In this example we store the order with its payment status in a database.
-                     */
-                    $model->setRetrievedMollieStatus($payment->status);
-                    $model->save();
-                    
-                    /*
-                     * Send the customer off to complete the payment.
-                     */
-                    $this->redirect($payment->getPaymentUrl());
-                    
-                }
-                catch (Mollie_API_Exception $e)
-                {
-                    echo "API call failed: " . htmlspecialchars($e->getMessage());
-                }
+            $model->setParameters();
+            if ($model->automatische_betaling) {
+                $model->createUser();
+            }
+            $payment = $model->createPayment();
+            if ($payment) {
+                $this->redirect($payment);
+            }
+        }
 
-            } else {
-                foreach ($model->errors as $key => $error) {
-                    Yii::$app->session->setFlash('warning', Yii::t('app', 'Fout met opslaan: ' . $key . ':' . $error[0]));
-                }
-            }
-        } else {
-            if(isset(Yii::$app->user->id)) {
-                $user = User::findOne(Yii::$app->user->id);
-            }
-            if (!isset($user)) {
-                $user = User::findByPayKey(Yii::$app->request->get('pay_key'));
-            }
-            if (!isset($user)) {
-                throw new NotFoundHttpException('Je bent niet ingelogt of de link uit je email is niet meer geldig.');
-            }
+        if(isset(Yii::$app->user->id)) {
+            $user = User::findOne(Yii::$app->user->id);
+        }
+
+        if (!isset($user)) {
+            $user = User::findByPayKey(Yii::$app->request->get('pay_key'));
+        }
+        
+        if (!isset($user)) {
+            throw new NotFoundHttpException('Je bent niet ingelogt of de link uit je email is niet meer geldig.');
         }
 
         $model->transacties_user_id = $user->id;
@@ -132,15 +93,19 @@ class MollieController extends TransactiesController
     public function actionWebhook()
     {
         $mollie = new Mollie;
+
+
+        /*
+         * Retrieve the payment's current state.
+         */
+        $payment = $mollie->mollie->payments->get(Yii::$app->request->post('id'));
+        $transacties_id = $payment->metadata->transacties_id;
+        $model = Transacties::findOne($transacties_id);
+        if ($payment->id !== $model->mollie_id) {
+            throw new NotFoundHttpException('The requested id does not correspond the database.');
+        }
         try
         {
-            /*
-             * Retrieve the payment's current state.
-             */
-            $payment = $mollie->mollie->payments->get(Yii::$app->request->post('id'));
-            $transacties_id = $payment->metadata->transacties_id;
-            $model = Transacties::findOne($transacties_id);
-
             /*
              * Update the transactie in the database.
              */
@@ -244,5 +209,50 @@ class MollieController extends TransactiesController
              Yii::$app->session->setFlash('primary', 'Log in om je overzicht te bekijken.');
         }
         return $this->render('/user/overzicht', ['model' => User::findOne(Yii::$app->user->id)]);
+    }
+
+    public function actionAutomatischBetalingUpdaten()
+    {
+        $user = new User;
+
+        if ($user->load(Yii::$app->request->post())) {
+            if(Yii::$app->request->get('actie') === 'annuleren') {
+                $user->automatische_betaling = FALSE;
+                $user->mollie_customer_id = '';
+                $user->mollie_bedrag = NULL;
+            }
+            if ($user->save()) {
+                if(Yii::$app->request->get('actie') === 'annuleren') {
+                    Yii::$app->session->setFlash('success', 'Automatisch ophogen is stop gezet.');
+                }
+                if(Yii::$app->request->get('actie') === 'annuleren') {
+                    Yii::$app->session->setFlash('success', 'Wijzeging in bedrag is opgeslagen.');
+                }
+            } else {
+                foreach ($modelBonnen->errors as $key => $error) {
+                    Yii::$app->session->setFlash('warning', Yii::t('app', 'Fout met opslaan: ' . $key . ':' . $error[0]));
+                }
+            }
+            if (!isset(Yii::$app->user->id)) {
+                Yii::$app->session->setFlash('primary', 'Log in om je overzicht te bekijken.');
+            }
+            return $this->render('/user/overzicht', ['model' => User::findOne(Yii::$app->user->id)]);
+        }
+        if(isset(Yii::$app->user->id)) {
+            $model = User::findOne(Yii::$app->user->id);
+        }
+
+        if (!isset($model)) {
+            $model = User::findByPayKey(Yii::$app->request->get('pay_key'));
+        }
+
+        if (!isset($model)) {
+            throw new NotFoundHttpException('Je bent niet ingelogt of de link uit je email is niet meer geldig.');
+        }
+        return $this->render('update', [
+            'model' => $model,
+            'actie' => 'update'
+        ]);
+
     }
 }
