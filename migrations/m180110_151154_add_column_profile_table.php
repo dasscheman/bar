a09b29d8ecfe80c68b94ba29aec4a8e45b811ca9
dasscheman <dasscheman@gmail.com>
2018-01-13 08:09:17 +0100

    Extra limit veld, voor automatische betalingen.

--- migrations/m180110_151154_add_column_profile_table.php
+++ migrations/m180110_151154_add_column_profile_table.php
@@ -9,13 +9,15 @@ class m180110_151154_add_column_profile_table extends Migration
         /*****************************************************************************/
         /* limit toegevoegd.
         /*****************************************************************************/
-        $this->addColumn('profile', 'limit', $this->money());
+        $this->addColumn('profile', 'limit_hard', $this->money());
+        $this->addColumn('profile', 'limit_ophogen', $this->money());
         $this->addColumn('profile', 'limit_bereikt', $this->integer());
     }
 
     public function down()
     {
-        $this->dropColumn('profile', 'limit');
+        $this->dropColumn('profile', 'limit_hard');
+        $this->dropColumn('profile', 'limit_ophogen');
         $this->dropColumn('profile', 'limit_bereikt');
 
     }
