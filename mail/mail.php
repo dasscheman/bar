096fcbbde802b299943344ae548879020b5180de
dasscheman <dasscheman@gmail.com>
2018-06-08 15:01:10 +0200

    Email aangepast

--- mail/mail.php
+++ mail/mail.php
@@ -16,19 +16,20 @@ use yii\helpers\Url;
 
         <br>
         Vanaf 1 maart is het niet meeer mogelijk om meer dan 20 euro rood te staan. Sta je meer dan 20 euro rood,
-        dan word je automatisch van de turflijst gehaald en kun je niets meer turven.
+        dan wordt je account automatisch bevroren, je kunt dan niets meer turven.
+        Je kunt je account pas weer gebruiken wanneer je betaald hebt.
         <br> <br>
-        Als je niet meer op de lijst staat, dan kun je er weer opkomen door je rekening te betalen.
-        Dit kun je doen door geld over te maken naar het bekende bisonbar rekeningnummer (staat in de bijlage).
-        Maar hier zit een flinke vertragen in omdat ik die betalingen niet vaker dan 1 keer per maand bijwerk.
-        <br>
-        Je kunt nu ook betalen met ideal. Het grote voordeel voor jou is dat deze betaling direct verwerkt wordt in het turfsysteem.
-        Ook kun je automatisch ophogen instellen, dan wordt je rekening opgehoogd met een door jou vast gesteld bedrag als je rekening onder de 0 euro komt.
-        <br>
-        <br>
-        <b>Let op!</b> de ideal betalingen zitten nog wel in een pilot fase.
-        Dus als er dingen niet goed gaan, niet gaan zoals je verwacht had of onduidelijk zijn, laat het me vooral even weten.
 
+        <b>Betalen</b>
+        <br>
+        Je kunt op verschillende manieren betalen. Als je me een plezier wilt doen, dan kun je betalen met de link hieronder.
+        Daarmee kun je een eenmalige IDEAL betaling doen, of een automatisch incasso aanmaken.
+        Deze betalingen worden direct verwerkt in het systeem en je tegoed wordt direct opgehoogd als de betaling succesvol is afgerond.
+        <br>
+        <br>
+        Je kunt ook nog steeds betalen door geld over te maken naar de Barrekening.
+        Dit heeft echter niet de voorkeur, want hier zit een flinke vertragen in omdat ik die betalingen niet vaker dan 1 keer per maand (met de hand) bijwerk.
+        Bovendien is dit extra werk voor mij :(
         <br>
         <br>
 
