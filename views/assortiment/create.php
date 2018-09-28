fd2b55f248d0ff5dff212be79b4032cb86d7ba63
dasscheman <dasscheman@gmail.com>
2018-09-28 21:38:48 +0200

    fixed inkoop

--- views/assortiment/create.php
+++ views/assortiment/create.php
@@ -4,19 +4,14 @@
  * Bar App de Bison by daan@biolgenkantoor.nl
  */
 
-use yii\helpers\Html;
+ use yii\helpers\Html;
 
-/**
-/* @var $this yii\web\View
- * @var $model app\models\Assortiment
- */
+ /* @var $this yii\web\View */
+ /* @var $model app\models\Inkoop */
 
+ $this->beginContent('../views/_beheer1.php');
 
-
-$this->beginContent('../views/_beheer2.php');
-                    <?php echo  $this->render('_form', ['model' => $model]); ?>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
+ echo $this->render('_form', [
+     'model' => $model,
+ ]);
+ $this->endContent();
